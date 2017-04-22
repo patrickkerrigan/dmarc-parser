2eed4a5596bd284e75e7a36ea0f94c2e2b171dea
Patrick Kerrigan <patrick@patrickkerrigan.uk>
2017-04-22 16:41:46 +0100

    Add additional tests for invalid emails

--- src/test/scala/uk/pkerrigan/dmarcparser/EmailParserSpec.scala
+++ src/test/scala/uk/pkerrigan/dmarcparser/EmailParserSpec.scala
@@ -1,27 +1,46 @@
 package uk.pkerrigan.dmarcparser
 
-import org.scalatest.FlatSpec
+import org.scalatest.{BeforeAndAfter, FlatSpec}
 import uk.pkerrigan.dmarcparser.report._
 
 import scala.io.Source
 
-class EmailParserSpec extends FlatSpec with ParserTrait {
+class EmailParserSpec extends FlatSpec with BeforeAndAfter with ParserTrait {
 
+  val dummyFeedback = Feedback(1.0, ReportMetadata("", "", "", "", DateRange(1,1), List()), PolicyPublished("", AlignmentRelaxed, AlignmentRelaxed, DispositionNone, DispositionNone, 0, ""), List())
   val emailParser = new EmailParser(this)
   var passedToParser: String = ""
 
+  before {
+    passedToParser = ""
+  }
+
   "An email with a zip file" should "invoke the DMARC parser with unzipped contents" in {
-    emailParser.parseEmail(Source.fromResource("test.zip.eml").mkString)
+    val result = emailParser.parseEmail(Source.fromResource("test.zip.eml").mkString)
     assert(passedToParser == "Test zip file")
+    assert(result.get == dummyFeedback)
   }
 
   "An email with a gzip file" should "invoke the DMARC parser with unzipped contents" in {
-    emailParser.parseEmail(Source.fromResource("test.gz.eml").mkString)
+    val result = emailParser.parseEmail(Source.fromResource("test.gz.eml").mkString)
     assert(passedToParser == "Test gzip file")
+    assert(result.get == dummyFeedback)
+  }
+
+  "An email with a an unrecognised attachment" should "not invoke the DMARC parser" in {
+    val result = emailParser.parseEmail(Source.fromResource("test.txt.eml").mkString)
+    assert(passedToParser == "")
+    assert(result.isEmpty)
+  }
+
+  "An email with no attachment" should "not invoke the DMARC parser" in {
+    val result = emailParser.parseEmail(Source.fromResource("test.none.eml").mkString)
+    assert(passedToParser == "")
+    assert(result.isEmpty)
   }
 
   override def parse(rawReport: String): Feedback = {
     passedToParser = rawReport.trim
-    Feedback(1.0, ReportMetadata("", "", "", "", DateRange(1,1), List()), PolicyPublished("", AlignmentRelaxed, AlignmentRelaxed, DispositionNone, DispositionNone, 0, ""), List())
+    dummyFeedback
   }
 }
